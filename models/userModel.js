5ecccad58ffb56c36105613a09480b0765c11395
abhijitmondall <abhijitmondal.cse@gmail.com>
2023-06-10 02:36:23 +0600

    User Model Added

---
+++ models/userModel.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: [true, 'Please tell us your name!'],
+  },
+
+  email: {
+    type: String,
+    required: [true, 'Please tell us your email!'],
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [validator.isEmail, 'Please provide a valid email!'],
+  },
+
+  photo: {
+    type: String,
+  },
+
+  role: {
+    type: String,
+    trim: true,
+    enum: {
+      values: ['student', 'instructor', 'admin'],
+      message:
+        'A Class must have a status either: (student, instructor, admin)!',
+    },
+    default: 'student',
+  },
+
+  classes: {
+    type: Number,
+    min: 0,
+  },
+
+  students: {
+    type: Number,
+    min: 0,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
