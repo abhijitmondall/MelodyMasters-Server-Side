fb1876ccb705b8742dabd5cf28618f5168201162
abhijitmondall <abhijitmondal.cse@gmail.com>
2023-06-10 02:53:27 +0600

    Code Refactor

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
+const userRouter = require('./routes/userRoutes');
 const classRouter = require('./routes/classRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -17,12 +18,17 @@ if (process.env.NODE_ENV === 'development') {
 app.use(cors());
 app.use(express.json());
 
+// User Route
+app.use('/api/v1/users', userRouter);
+
+// Class Route
 app.use('/api/v1/classes', classRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on the server!`, 404));
 });
 
+// Global Error Handler Middleware
 app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ controllers/userController.js
@@ -0,0 +1,73 @@
+const catchAsync = require('../utils/catchAsync');
+const User = require('./../models/userModel');
+
+exports.getAllUsers = catchAsync(async (req, res) => {
+  const users = await User.find();
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+});
+
+exports.getUser = catchAsync(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user)
+    return next(
+      new AppError(`No User found with this ID: ${req.params.id}`, 404)
+    );
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user,
+    },
+  });
+});
+
+exports.createUser = catchAsync(async (req, res) => {
+  const user = await User.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      user,
+    },
+  });
+});
+
+exports.updateUser = catchAsync(async (req, res) => {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!user)
+    return next(
+      new AppError(`No User found with this ID: ${req.params.id}`, 404)
+    );
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user,
+    },
+  });
+});
+
+exports.deleteUser = catchAsync(async (req, res) => {
+  const user = await User.findByIdAndDelete(req.params.id);
+
+  if (!user)
+    return next(
+      new AppError(`No User found with this ID: ${req.params.id}`, 404)
+    );
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
